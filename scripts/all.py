803b3bf67e577afd86944a8f7ce7fc31ea38b2f6
lx <445462184@qq.com>
2025-02-24 23:41:53 +0800

    全部脚本修改

--- scripts/all.py
+++ scripts/all.py
@@ -11,11 +11,15 @@ def main():
     # 假设我们有不同的参数设置列表
     param_sets = [
         # 第一个参数集
-        ["--task_name", "task1", "--session_name", "session1", "--epochs", "10", "--lr", "1e-4"],
+        ["--task_name", "masc", "--num_labels", "3", "--strategy", "none", "--session_name", "masc_1", "--mode", "text_only"],
         # 第二个参数集
-        ["--task_name", "task2", "--session_name", "session2", "--epochs", "20", "--lr", "1e-5"],
+        ["--task_name", "mate", "--session_name", "mate_1", "--pretrained_model_path", "checkpoints/model_1.pt", "--output_model_path", "checkpoints/model_mate.pt", "--num_labels", "3", "--strategy", "none", "--epochs", "20", "--lr", "1e-5", "--mode", "text_only"],
         # 可以添加更多的参数集
-        ["--task_name", "task3", "--session_name", "session3", "--epochs", "15", "--lr", "5e-5"]
+        ["--task_name", "mner", "--session_name", "mner_1", "--pretrained_model_path", "checkpoints/model_mate.pt", "--output_model_path", "checkpoints/model_mner.pt", "--train_text_file", "data/MNER/twitter2015/train.txt", "--test_text_file", "data/MNER/twitter2015/test.txt", "dev_text_file", "data/MNER/twitter2015/dev.txt", "image_dir", "data/MNER/twitter2015/images", "num_labels", "9", "strategy", "none", "mode", "text_only"],
+
+        ["--task_name", "mnre", "session_name", "mnre_1", "pretrained_model_path", "checkpoints/model_mner.pt", "output_model_path", "checkpoints/model_mnre.pt", "train_text_file", "data/MNRE/mix/mnre_txt/train.txt", "test_text_file", "data/MNRE/mix/mnre_txt/test.txt", "dev_text_file", "data/MNRE/mix/mnre_txt/dev.txt", "image_dir", "data/MNRE/mix/mnre_image", "num_labels", "23", "strategy", "none"],
+
+        ["--task_name", "mabsa", "num_labels", "7", "strategy", "none", "pretrained_model_path", "checkpoints/model_mnre.pt", "output_model_path", "checkpoints/model_mabsa.pt", "session_name", "mabsa_1", "mode", "text_only"]
     ]
 
 
